c3ffaf5fb2ed835882610ba21b662362b044c24d
jaycode2 <dashncom@naver.com>
2022-01-21 19:01:50 +0900

    Revert "220121 commit"
    This reverts commit 3eec9411349f3456ee4b03c3dfc69ce76bb50bd1.

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
-from .models import MainContent, Comment
+from .models import MainContent
 
-class MainContentAdmin(admin.ModelAdmin):
-    list_display = ['title', 'content', 'pub_date']
-    search_fields = ['title']
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ['content_list', 'content', 'author', 'create_date', 'modify_date']
-    search_fields = ['author']
-
-admin.site.register(MainContent, MainContentAdmin)
-admin.site.register(Comment, CommentAdmin)
+admin.site.register(MainContent)
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,8 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('<int:content_id>/', views.detail, name='detail'),
-
-    path('comment/create/<int:content_id>/', views.comment_create, name='comment_create'),
-    path('comment/update/<int:comment_id>/', views.comment_update, name='comment_update'),
-    path('comment/delete/<int:comment_id>/', views.comment_delete, name='comment_delete'),
 ]
